b512247e1782db55d918b04ecbaa8d8ee30f95fe
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-10-06 21:29:18 -0700

    Grunt task to copy binaries. Cleaned up grunt file.

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,40 @@
 module.exports = function(grunt) {
+
+    // Config variables.
+    var projectName = "uformsia";
+    var appProject = projectName + ".app";
+
+    // Initialize Grunt tasks.
     grunt.initConfig({
         "pkg": grunt.file.readJSON('package.json'),
         copy: {
             main: {
                 files: [
                     {
+                        // Frontend files.
                         expand: true,
                         src: ["App_Plugins/**"],
                         dest: 'Website/',
-                        cwd: "uformsia.app/"
+                        cwd: appProject + "/"
+                    }, {
+                        // Binaries.
+                        expand: true,
+                        src: [
+                            appProject + ".dll",
+                            appProject + ".pdb"
+                        ],
+                        dest: 'Website/bin/',
+                        cwd: appProject + "/bin/Debug/"
                     }
                 ]
             }
         }
     });
+
+    // Load NPM tasks.
     grunt.loadNpmTasks("grunt-contrib-copy");
+
+    // Register Grunt tasks.
     grunt.registerTask("default", ["copy"]);
+
 };

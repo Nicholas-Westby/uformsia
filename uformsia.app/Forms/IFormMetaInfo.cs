0d33586acc9f82983796115a9dfbf446a6943ad8
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2015-10-11 16:13:15 -0700

    Began implementing form storage.

---
+++ uformsia.app/Forms/IFormMetaInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace uformsia.app.Forms
+{
+
+    // Namespaces.
+    using System;
+
+
+    /// <summary>
+    /// The interface for all form meta information.
+    /// </summary>
+    public interface IFormMetaInfo
+    {
+        string Alias { get; set; }
+        string Name { get; set; }
+        Type ValueType { get; }
+    }
+
+}
